66129c392806dd66ca209716c1221f075bf28107
MAKLs <mikeashimko@gmail.com>
2022-10-22 11:33:26 -0400

    refactor for test

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
 #![forbid(unsafe_code)]
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health", web::get().to(health_check)))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    run()?.await
 }
