4bd5a165ae30afbd8b11dbb918a9e46ec8e30e90
MAKLs <mikeashimko@gmail.com>
2022-10-30 10:19:12 -0400

    add telemetry module

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,6 @@
 pub mod configuration;
 mod routes;
 mod startup;
+pub mod telemetry;
 
 pub use startup::{run, run_with_listener};
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 #![forbid(unsafe_code)]
 use sqlx::PgPool;
-use zero2prod::{configuration::get_configuration, run};
+use zero2prod::{
+    configuration::get_configuration,
+    run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber(env!("CARGO_BIN_NAME"), "info");
+    init_subscriber(subscriber);
     let configuration = get_configuration().expect("failed to read configuration");
     let pool = PgPool::connect(&configuration.database.connection_string())
         .await
