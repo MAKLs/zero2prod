fc13f124fd86a4c81ef1fbe5cb080f0828ef6667
MAKLs <mikeashimko@gmail.com>
2022-11-06 17:26:37 -0500

    switch to tracing-actix-web

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,8 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, middleware::Logger, web, App, HttpServer};
+use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
+use tracing_actix_web::TracingLogger;
 
 use crate::routes;
 
@@ -17,7 +18,7 @@ pub fn run_with_listener(listener: TcpListener, pool: PgPool) -> std::io::Result
     let pool = web::Data::new(pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .app_data(pool.clone())
             .route("/health", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
